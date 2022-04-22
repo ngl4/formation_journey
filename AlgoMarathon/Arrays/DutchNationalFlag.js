dd8b1a03de7f32c57d2c64ca084cffdea0fe53d5
Cindy Lam <cindybinh444@gmail.com>
2022-04-21 21:13:35 -0400

    Added notes on dutch national flag algo

---
+++ AlgoMarathon/Arrays/DutchNationalFlag.js
@@ -0,0 +1,101 @@
+/**
+  ▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁
+  ✏️ Description
+  ▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+  Q. Given an array of 0s, 1s, and 2s, sort them in-place in ascending order.
+  
+  Examples:
+  • Given an array: [2, 1] // returns [1, 2]
+  • Given an array: [0, 2, 1, 0, 1, 2] // returns [0, 0, 1, 1, 2, 2]
+  
+  
+  Visualization 
+  
+  0, 0, 1, 1, 2, 2
+  ^ (do not move at all)
+     ^ (only move when it is swapping with numbers on the right side)
+        ^ (move every iteration)
+  
+  Approaches: 
+  - .sort() method. Time: O(nlogn) 
+  -  bubble sort method. Time: O(n^2)
+  -  one pass Time: O(n)
+  
+   */
+
+  //ATTEMPT #1 (unsure): Walking through the problem with Alexis!!
+  
+  function dnf(input) {
+
+
+    let movableP2 = false; 
+    let p1 = input[0]; 
+    let p2 = input[1];   
+    let inc = 1; 
+    for(let i = 2; i < input.length; i++) {
+      let p3 = input[i]; 
+      if (p2 < p1) {
+        [p2, p1] = [p1, p2];
+        movableP2 = true; 
+      }
+      if (p3 < p2) {
+        [p3, p2] = [p2, p3];
+      }
+      if (movableP2) {
+        inc += 1; 
+        p2 = input[inc]; 
+        movableP2 = false; 
+      }
+    }
+    return input;
+  
+    // TEST CASES PASSED - BUBBLE SORT METHOD
+    // for(let i = 0; i < input.length; i++){
+    //   for (let j = 0; j < input.length; j++) {
+    //     const curVal = input[j]; 
+    //     const nextVal = input[j+1]; 
+    //     if (nextVal < curVal) {
+    //       [input[j], input[j+1]]=[nextVal, curVal];
+    //     }
+    //   }
+    // }
+    // return input; 
+  }
+  
+  // Test Cases
+  test.startProblem("Dutch National Flag")
+  var array1 = []; dnf(array1)
+  var array2 = [2, 1, 1, 0]; dnf(array2)
+  var array3 = [0, 2, 1, 0, 1, 2]; dnf(array3)
+  test.testForArrays([], array1, 1)
+  test.testForArrays([0, 1, 1, 2], array2, 2)
+  test.testForArrays([0, 0, 1, 1, 2, 2], array3, 3)
+  test.endProblem()
+
+
+  /*
+  PROGRESS NOTE
+
+  14m - basic understanding the prob, visualization, using bubble sort to solve the problem first
+
+  16m 
+  - trying to use a 3 pointer approach 
+  - working on the visualization for around 5-10mins 
+  - Then use the rest of the code to code out the solution 
+  - It is not successful 
+
+  Reflection: 
+  - I am grateful that Alexis is able to work together with me on this problem 
+  - She has given me some hints on how I could approach this problem ~ using 3 pointers 
+  
+  Future Attempts: 
+  Walk through the 3 commons solutions to this problem:
+• Sort the array as just a normal unsorted array. 
+• Two passes:
+    First pass: count the number of 0s, 1s and 2s. 
+    Second pass: overwrite the original array with the correct number of 0s, then 1s, then 2s.
+    
+• One pass (read about this on Leetcode)
+
+  */
+  
--- AlgoMarathon/README.md
+++ AlgoMarathon/README.md
@@ -10,6 +10,8 @@ Welcome to Level 2 of Algo Blitz!!
 
 - Follow through the Formation Tasks, and practice more on Neetcode, Leetcode (sort by problem type and acceptance rate), AlgoExpert if needed.
 
+- Try to walk through the problem in "English" to understand what it is asking
+
 _Special thanks to a Formation Fellow Dana Ng who has introduced me to keep track of my progress using Attempts table_
 
 ## Fundamentals 
@@ -65,6 +67,18 @@ https://www.geeksforgeeks.org/convert-array-into-zig-zag-fashion/
 Attempt#1: includes time that I watched the Youtube video (8mins)
 
 [Details here](https://github.com/ngl4/formation_journey/blob/main/AlgoMarathon/Arrays/ConvertArrToZigzag.js)
+
+
+#### Learn Strength Algo: Dutch National Flag
+
+| Attempt | #1  | #2  | #3  | #4  | #5  | #6  | #7  |
+| :-----: | :-: | :-: | :-: | :-: | :-: | :-: | :-: |
+| Minutes | 30  | --  | --  | --  | --  | --  | --  |
+
+Attempt#1: spent an extra 15 mins to read through the solution in Leetcode regarding the One Pass approach to this problem. 
+
+[Details here]()
+
 
 
 <!--
