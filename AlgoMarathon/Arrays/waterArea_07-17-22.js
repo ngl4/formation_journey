592e4b10d9100b5f67da3213f63b2672d990856a
Cindy Lam <cindybinh444@gmail.com>
2022-07-18 23:54:15 -0400

    Added notes 07-18-22

--- AlgoMarathon/Arrays/waterArea_07-17-22.js
+++ AlgoMarathon/Arrays/waterArea_07-17-22.js
@@ -177,5 +177,7 @@
   }
 
   console.log(findWaterArea([3, 0, 2, 0, 4])) //7
-  // console.log(findWaterArea([3, 2, 1, 2, 3])) //4
-  // console.log(findWaterArea([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])) //6
+  console.log(findWaterArea([5, 0, 0, 0, 0, 1])) //4
+  console.log(findWaterArea([1, 0, 2, 0, 3])) //3
+  console.log(findWaterArea([3, 2, 1, 2, 3])) //4
+  console.log(findWaterArea([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])) //6
