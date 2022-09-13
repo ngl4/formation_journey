0c6ef7bea97240da960297d7499161242bdd67f7
Cindy Lam <cindybinh444@gmail.com>
2022-09-12 22:50:26 -0400

    Added notes 09/12/22

---
+++ AlgoMockedInterview/Basics_Self_Practice/Recursion/fib.js
@@ -0,0 +1,32 @@
+/*
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+Given n, calculate F(n).
+*/
+
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+/*
+
+3 conditions: 
+1) f(n) = f(n-1) + f(n - 2) only when n >= 2 
+2) f(1) === 1 
+3) f(0) === 0
+
+fib(2) = fib(1) + fib(0) 
+= 1 + 0 = 1
+
+
+*/
+var fib = function(n) {
+    if (n === 0) return 0;
+    if (n >= 2) {
+        return fib(n - 1) + fib(n - 2); 
+    }
+    return 1; 
+};
